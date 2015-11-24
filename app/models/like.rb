d3dff0ea2103c0b424aeafe48c6d2b5ea5f71dfa
RamziAssaf <rassaf@chicagobooth.edu>
2015-11-23 21:13:31 -0600

    HW

---
+++ app/models/like.rb
@@ -0,0 +1,11 @@
+class Like < ActiveRecord::Base
+
+	# Associations:
+	belongs_to :user
+	belongs_to :photo
+
+	# Validations:
+	validates :user_id, :presence => true, :uniqueness => { :scope => :photo_id }
+	validates :photo_id, :presence => true
+
+end
